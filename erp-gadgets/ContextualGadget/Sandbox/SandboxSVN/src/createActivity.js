c4298166c62847ccba244e188975d7ed730cc506
Venkatesanb <venkatesan.ece@gmail.com>
2015-05-29 18:14:54 +0530

    Update createActivity.js
    cahnged

--- erp-gadgets/ContextualGadget/Sandbox/SandboxSVN/src/createActivity.js
+++ erp-gadgets/ContextualGadget/Sandbox/SandboxSVN/src/createActivity.js
@@ -344,6 +344,7 @@ try
  //	activitySaveCount=5;
   if(activitySaveCount!=5 && (obj.text==null||obj.text==""))
   {
+    activitySOAPAction='rpc/http://siebel.com/CustomUI:ANSActivityThinCustomUIQueryPage_Input';
     setTimeout(invokeSiebeWebservice(querydata,activitySOAPAction,'activityResponse'), 9000);
      activitySaveCount =activitySaveCount+1;
   }
